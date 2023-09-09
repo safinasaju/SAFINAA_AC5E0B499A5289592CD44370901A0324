37996772028f46c6f527c46cdf0eaaef98590bf5
safinasaju <144546323+safinasaju@users.noreply.github.com>
2023-09-09 21:43:50 +0530

    Unit 1 challenge

---
+++ main (1).py
@@ -0,0 +1,9 @@
+
+year=int(input("enter the year:"))
+if(year%400==0)and(year%100==0):
+  print(year,"is leap year")
+  elisf(year % 400==0)and (year % 100==0)
+  print(year,"is leap year")
+else:
+
+  print(year,"is not leap year")
---
+++ main.py
@@ -0,0 +1,8 @@
+def factorial(n):
+  if n==0 or n==1:
+    return 1
+  else:
+    return n*factorial(n-1)
+number = 2
+result = factorial(number)
+print("the factorial of{}  is {}.".format(number,result))
